e625ff6067598edd668c027ef586b5bd204baf96
Brian Simoni <simonibc@vcu.edu>
2015-11-30 18:26:50 -0500

    Added cd command

--- shell.go
+++ shell.go
@@ -1,38 +1,65 @@
 // runs the program, prints out the currenct directory + dollar sign
 // waits for user input
-// commands that work right now: ls, exit
 // if command not found, print "No command (command) found"
+//
+//
+// ****** commands that work right now: ls, exit, cd ******
+
 package main
 
 import (
     "fmt"
     "io/ioutil"
     "path/filepath"
+    "os"
+    "bufio"
+    "strings"
 )
 
 func main() {
 
+	path, err := filepath.Abs("/")
+	os.Chdir(path)
+
 	exit := false
 	for exit == false {
 
-		root, _ := filepath.Abs(".")
-		fmt.Print(root,"$ ")
+		
+		fmt.Print(path ,"$ ")
 
-		input := ""
-		fmt.Scanln(&input)
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		slice := strings.Split(input, " ")
 
-		if input == "ls" {
+		fmt.Println(slice)
 
-			files, _ := ioutil.ReadDir("./")
+		command := slice[0]
+		var arg string
+
+		if len(slice) > 1{
+			arg = slice[1]
+		}
+
+
+		if command == "dir" {
+
+			files, _ := ioutil.ReadDir(path)
 			for _, f := range files {
 	    		fmt.Println(f.Name())
 	    	}
 
-		} else if input == "exit" {
+		} else if command == "cd" {
+			path, err = filepath.Abs(arg)
+			os.Chdir(path)
+			if err != nil{
+				fmt.Println(err)
+			}
+		} else if command == "exit" {
 			exit = true
 			fmt.Println("Go shell exited")
 		} else {
-			fmt.Println("No command",input,"found")
+			fmt.Println("No command",command,"found")
 		}
 
 	}
